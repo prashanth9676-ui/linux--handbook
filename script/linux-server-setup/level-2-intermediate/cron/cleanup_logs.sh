7691e3c4a1723d32b75fd4566dc311124607f4a8
prashanth9676-ui <saip59921@gmail.com>
2025-12-02 15:07:46 +0530

    Update cleanup_logs.sh

--- script/linux-server-setup/level-2-intermediate/cron/cleanup_logs.sh
+++ script/linux-server-setup/level-2-intermediate/cron/cleanup_logs.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-LOG_DIR=/srv/myapp/logs
+LOG_DIR="/var/log/myapp"
 
-find "$LOG_DIR" -type f -mtime +14 -print -delete
+find $LOG_DIR -name "*.log" -mtime +7 -delete
 
-for f in "$LOG_DIR"/*.log; do
-  [ -f "$f" ] || continue
-  if [ $(stat -c%s "$f") -gt $((100*1024*1024)) ]; then
-    mv "$f" "$f.$(date +%F_%H%M)"
-    touch "$f"
-  fi
-done
+echo "Old log files deleted at $(date)"
+
